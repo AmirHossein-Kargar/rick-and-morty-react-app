d3189973afc56a63fc0b62d3311e23566e3afb12
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-12-05 13:37:49 +0330

    feat: Implement CharacterDetails component with dynamic data rendering

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -1,5 +1,55 @@
+import { ArrowUpCircleIcon } from "@heroicons/react/24/outline";
+import { character, episodes } from "../../data/data";
+
 export default function CharacterDetails() {
   return (
-    <div>CharacterDetails</div>
-  )
+    <div style={{ flex: 1 }}>
+      <div className="character-detail">
+        <img
+          src={character.image}
+          alt={character.name}
+          className="character-detail__img"
+        />
+        <div className="character-detail__info">
+          <h3 className="name">
+            <span>{character.gender === "Male" ? "👦🏼" : "🙍🏼‍♀️"}</span>
+            <span>&nbsp;{character.name}</span>
+          </h3>
+          <div className="info">
+            <span
+              className={`status ${character.status === "Dead" ? "red" : ""}`}
+            ></span>
+            <span>&nbsp; {character.status}</span>
+            <span>&nbsp;- {character.species}</span>
+          </div>
+          <div className="location">
+            <p>Last Known Location:</p>
+            <p>{character.location.name}</p>
+          </div>
+          <div className="actions">
+            <button className="btn btn--primary">Add To Favorite</button>
+          </div>
+        </div>
+      </div>
+      <div className="character-episodes">
+        <div className="title">
+          <h2>List Of Episodes:</h2>
+          <ArrowUpCircleIcon className="icon" />
+        </div>
+        <ul>
+          {episodes.map((item, index) => {
+            return (
+              <li key={item.id}>
+                <div>
+                  {String(index + 1).padStart(2, 0)} - {item.episode} :{" "}
+                  <strong>{item.name}</strong>
+                </div>
+                <div className="date badge badge--secondary">{item.air_date}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
